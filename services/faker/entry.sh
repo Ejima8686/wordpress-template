b2ebceacfd9099222ad46c8c517d8e95dd199471
ejima8686 <ejima@nevers.jp>
2025-06-06 14:21:48 +0900

    🐛 Fix: プラグインと同一の機能削除

--- services/faker/entry.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script_dir=$(dirname "$0")  
-
-if [ ! -d "$script_dir/vendor" ]; then
-    cd $script_dir && composer install 
-fi
-
-php "$script_dir/setup-dummy-posts.php" --allow-root
