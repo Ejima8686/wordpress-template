aa0aae3779f43c0b3e1f3fcf2e933afa3cfac8c9
ejima8686 <ejima@nevers.jp>
2025-07-29 17:07:53 +0900

    🐛 Fix: ホットリロードが効かない問題の改善

--- mytheme/inc/timber/context.php
+++ mytheme/inc/timber/context.php
@@ -3,6 +3,8 @@
 use Timber\Timber;
 
 add_filter("timber/context", function ($context) {
+	$context["IS_DEVELOPMENT"] = is_dev();
+	$context["IS_VITE_RUNNING"] = check_vite_connection();
 	$context["options"] = get_fields("options");
 	$context["about_post"] = Timber::get_post([
 		"name" => "about",
